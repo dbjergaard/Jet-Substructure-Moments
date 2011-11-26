3e0991a3f49b0d01604554949d58977532db9d04
David Bjergaard <dbjergaard@gmail.com>
2011-11-26 13:11:32 -0500

    Restructure code

--- JetColor_LinkDef.h
+++
@@ -1,6 +0,0 @@
-#ifdef __CINT__
-#pragma link C++ class std::vector<float>+;
-//#pragma link C++ class AttributeListLayout+;
-//#pragma link C++ class TTreeIndex+;
-#pragma link C++ class std::pair<std::string,std::string>+;
-#endif 
